7b4e206a138c34cd937a388e68780db99aedbb76
Tomader-Alshehiween <toma.f.alshehiween@gmail.com>
2024-08-05 11:25:33 +0300

    first work

---
+++ app/Filament/Resources/CircuitResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CircuitResource\Pages;
+use App\Filament\Resources\CircuitResource\RelationManagers;
+use App\Models\Circuit;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CircuitResource extends Resource
+{
+    protected static ?string $model = Circuit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cpu-chip';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCircuits::route('/'),
+            'create' => Pages\CreateCircuit::route('/create'),
+            'edit' => Pages\EditCircuit::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/EntityName.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EntityName extends Model
+{
+    use HasFactory;
+    protected $table = 'entity_name';
+    protected $fillable = [
+        'entity_name',
+        'entity_type_id'
+        ];
+}
